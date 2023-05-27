403c7f598d6d761f25e6cd98ff4aadba59e16169
Saidafzalkhon <Saidafzalkhon.developer@gmail.com>
2023-05-27 19:27:34 +0500

    Complete Government_service

--- src/main/java/uz/java/music/repository/GovernmentRepository.java
+++ src/main/java/uz/java/music/repository/GovernmentRepository.java
@@ -1,7 +1,17 @@
 package uz.java.music.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import uz.java.music.entity.Government_service;
 
 public interface GovernmentRepository extends JpaRepository<Government_service,Long> {
+    @Modifying
+    @Query("UPDATE Government_service g SET g.link = :link WHERE g.id = :id")
+    void updateLink(@Param("id") Long id, @Param("link") String link);
+
+    @Modifying
+    @Query("DELETE FROM Government_service g WHERE g.id = :id")
+    void deleteAdmin(@Param("id") Long id);
 }
--- src/main/java/uz/java/music/service/impl/Government_serviceServiceImpl.java
+++ src/main/java/uz/java/music/service/impl/Government_serviceServiceImpl.java
@@ -5,15 +5,22 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.InvalidDataAccessResourceUsageException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import uz.java.music.dto.Government_serviceDto;
+import uz.java.music.entity.Admin;
+import uz.java.music.entity.Government_service;
 import uz.java.music.exception.Duplicate;
+import uz.java.music.exception.NotFound;
 import uz.java.music.exception.NotSaved;
 import uz.java.music.repository.GovernmentRepository;
 import uz.java.music.service.Government_serviceService;
 import uz.java.music.service.mapper.GovernmentMapper;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class Government_serviceServiceImpl implements Government_serviceService {
 
     @Autowired
@@ -24,30 +31,60 @@ public class Government_serviceServiceImpl implements Government_serviceService 
     public ResponseEntity<Government_serviceDto> add(Government_serviceDto serviceDto) {
         try{
             return new ResponseEntity<>(mapper.toDto(repository.save(mapper.toEntity(serviceDto))), HttpStatus.CREATED);
-        }catch (InvalidDataAccessResourceUsageException e){
-            throw new NotSaved("Admin not saved");
-        }catch (DataIntegrityViolationException e){
-            throw new Duplicate("Admin username already exists");
+        }catch (InvalidDataAccessResourceUsageException e) {
+            throw new NotSaved("Government service not saved");
         }
     }
 
     @Override
     public ResponseEntity<Government_serviceDto> update(Government_serviceDto serviceDto) {
-        return null;
+            if(serviceDto.getId() == null){
+                throw new NotFound("Government service is not found");
+            }else {
+                if (repository.findById(serviceDto.getId()).isPresent()) {
+                    return new ResponseEntity<>(mapper.toDto(repository.save(mapper.toEntity(serviceDto))), HttpStatus.OK);
+                } else {
+                    throw new NotSaved("Government service not updated");
+                }
+            }
     }
-
+    @Transactional
     @Override
     public ResponseEntity<Government_serviceDto> update_link(Long id, String link) {
-        return null;
+        try{
+            repository.updateLink(id,link);
+            Optional<Government_service> service = repository.findById(id);
+            if(service.isPresent()){
+                return new ResponseEntity<>(mapper.toDto(service.get()),HttpStatus.OK);
+            }else{
+                throw new NotSaved("Government service name not updated");
+            }
+        }catch (InvalidDataAccessResourceUsageException e){
+            throw new NotSaved("Government service not updated");
+        }
     }
 
     @Override
     public ResponseEntity<List<Government_serviceDto>> getAll() {
-        return null;
+        try {
+            return new ResponseEntity<>(repository.findAll().stream().map(mapper::toDto).toList(), HttpStatus.OK);
+        } catch (InvalidDataAccessResourceUsageException e) {
+            throw new NotSaved("Database not connected");
+        }
     }
-
+    @Transactional
     @Override
     public ResponseEntity<Government_serviceDto> delete(Long id) {
-        return null;
+        Optional<Government_service> service = repository.findById(id);
+        if(service.isPresent()){
+            try{
+                repository.deleteAdmin(id);
+                return new ResponseEntity<>(mapper.toDto(service.get()),HttpStatus.OK);
+            }catch (Exception e){
+                throw new NotSaved("Government service not deleted");
+            }
+        }else{
+            throw new NotFound("Government service is not found");
+        }
     }
 }
